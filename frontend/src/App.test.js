9b9dad641b1245edb1f7adc447db2622a85b28ca
katyyy111 <kasiakuzora@gmail.com>
2025-11-26 10:53:22 +0100

    changed the technology for vite - backend changes in progress

--- frontend/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders message from backend', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Loading message from backend/i);
-  expect(linkElement).toBeInTheDocument();
-});
