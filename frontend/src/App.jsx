d9be6422f5292048ba7a3742f0d8f2a83216f481
katyyy111 <kasiakuzora@gmail.com>
2025-11-26 10:55:19 +0100

    better organization

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,8 @@
 import './App.css';
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import UserProfile from './UserProfile';
-import Home from './Home';
+import UserProfile from './pages/UserProfile';
+import Home from './pages/Home';
 
 function App() {
   /*
