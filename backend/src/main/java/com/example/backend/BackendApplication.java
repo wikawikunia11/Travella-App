8bba4f031e98859bbf27671ea4ab51f326e37f66
Wiktoria Małażewska <01197214@pw.edu.pl>
2025-11-27 16:34:28 +0100

    added swagger

--- backend/src/main/java/com/example/backend/BackendApplication.java
+++ backend/src/main/java/com/example/backend/BackendApplication.java
@@ -2,11 +2,10 @@ package com.example.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.beans.factory.annotation.Autowired; 
+import org.springframework.beans.factory.annotation.Autowired;
 import java.util.Optional;
 
 @SpringBootApplication
@@ -19,23 +18,21 @@ public class BackendApplication {
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
-	@CrossOrigin(origins = "http://localhost:5173")
 	@GetMapping("/api/message")
-	public String getMessage() {
-		return "Hello from the backend! balbinka wita";
-	}
+    public String getMessage() {
+        return "Hello from the backend! balbinka wita";
+    }
 
-	@CrossOrigin(origins = "http://localhost:5173")
     @GetMapping("/api/users")
     public Iterable<User> getAllUsers() {
         return userRepository.findAll();
     }
 
-	@CrossOrigin(origins = "http://localhost:5173")
     @GetMapping("/api/users/{id}")
     public User getUser(@PathVariable Long id) {
-        // Używamy findById, aby zapytać bazę
+        //zabezpieczenie przed błędem 500
         Optional<User> userOptional = userRepository.findById(id);
         return userOptional.orElse(null);
-	}
+    }
+
 }
