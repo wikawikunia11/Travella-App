2e0721c571ee017a7b3e33cf0dd5eab35f2312ca
Gab-Master <Gabryel.jun@gmail.com>
2025-11-27 20:34:40 +0100

    user profile

--- backend/src/main/java/com/example/backend/User.java
+++ backend/src/main/java/com/example/backend/User.java
@@ -5,12 +5,14 @@ public class User {
     private String name;
     private String nickname;
     private String email;
+    private String biography;
 
-    public User(Long id, String name, String nickname, String email) {
+    public User(Long id, String name, String nickname, String email, String biography) {
         this.id = id;
         this.name = name;
         this.nickname = nickname;
         this.email = email;
+        this.biography = biography;
     }
 
     // gettery i settery
@@ -18,9 +20,11 @@ public class User {
     public String getName() { return name; }
     public String getNickname() { return nickname; }
     public String getEmail() { return email; }
+    public String getBiography() { return biography; }
 
     public void setId(Long id) { this.id = id; }
     public void setName(String name) { this.name = name; }
     public void setNickname(String nickname) { this.nickname = nickname; }
     public void setEmail(String email) { this.email = email; }
+    public void setBiography(String biography) { this.biography = biography; }
 }
