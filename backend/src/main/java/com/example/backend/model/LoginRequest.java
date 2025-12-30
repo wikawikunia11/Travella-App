52ecd19e7127dcd06d63f3313142e61a5bf7c807
Wiktoria Małażewska <01197214@pw.edu.pl>
2025-12-30 12:15:41 +0100

    fixed lombok config

--- backend/src/main/java/com/example/backend/model/LoginRequest.java
+++ backend/src/main/java/com/example/backend/model/LoginRequest.java
@@ -1,16 +1,12 @@
 package com.example.backend.model;
 
+import lombok.Data;
+
+@Data
 public class LoginRequest {
     private String username;
     private String password;
 
     // JPA needed blanck constructor
     public LoginRequest() {}
-
-    // getters and setters
-    public String getUsername() {return username;}
-    public void setUsername(String username) {this.username = username;}
-
-    public String getPassword() { return password;}
-    public void setPassword(String password) {this.password = password;}
 }
--- backend/src/main/java/com/example/backend/model/User.java
+++ backend/src/main/java/com/example/backend/model/User.java
@@ -7,6 +7,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -40,9 +41,16 @@ public class User {
     @Column(name = "biography", length = 180)
     private String biography;
 
-    @Column(name = "creation_date", updatable = false) // updatable=false, creation date cannot be changed
+    @Column(name = "creation_date", updatable = false)
     private LocalDateTime creationDate;
 
     @Column(name = "profile_pic", length = 250)
     private String profilePic;
+
+    @PrePersist
+    protected void onCreate() {
+        if (this.creationDate == null) {
+            this.creationDate = LocalDateTime.now();
+        }
+    }
 }
