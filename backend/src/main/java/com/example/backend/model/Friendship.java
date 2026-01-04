c9f0ff7bbc303bb41995e3de36b4dc0fb93e862e
Weronika Grzybowska <01197475@pw.edu.pl>
2026-01-04 20:07:25 +0100

    added constructors

--- backend/src/main/java/com/example/backend/model/Friendship.java
+++ backend/src/main/java/com/example/backend/model/Friendship.java
@@ -9,9 +9,11 @@ import jakarta.persistence.IdClass;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Entity
+@NoArgsConstructor
 @IdClass(FriendshipId.class)
 @Table(name = "friendships")
 public class Friendship {
@@ -29,5 +31,15 @@ public class Friendship {
     @PrePersist
     protected void onCreate() {
         if (this.creationDate == null) this.creationDate = LocalDateTime.now();
+    }
+
+    public Friendship(Long id1, Long id2) {
+        if (id1 < id2) {
+            this.firstUserId = id1;
+            this.secondUserId = id2;
+        } else {
+            this.firstUserId = id2;
+            this.secondUserId = id1;
+        }
     }
 }
