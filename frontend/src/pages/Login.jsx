d44e05a6301315a5cb759f3b67dd02bc1ecb71b1
Wiktoria Małażewska <01197214@pw.edu.pl>
2026-01-13 22:07:01 +0100

    Revert "Merge branch 'develop' of https://gitlab-stud.elka.pw.edu.pl/wmalazew/pap2025z-z13 into develop"
    This reverts commit 1315c5f6e56d85656f1d9375a8f5163d94d878ad

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useUser } from '../UserContext';
 import styles from './Login.module.css';
@@ -11,7 +11,7 @@ function Login() {
    const [type, setType] = useState('password');
    const [off, setOff] = useState(true);
    const navigate = useNavigate();
-   const { user, login } = useUser();
+   const { login } = useUser();
 
     function handleLogin(e) {
         e.preventDefault();
@@ -51,12 +51,6 @@ function Login() {
             setType('password')
         }
     }
-
-    useEffect(() => {
-        if (user !== null) {
-            navigate(`/profile/${user.username}`);
-        }
-    });
 
     return (
     <div className={styles.root}>
